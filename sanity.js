5c06c8da00a78c0adc87b429ab75612dc484b917
dbfl4524 <sinyuri0525@gmail.com>
2024-03-26 12:27:27 +0900

    chapter6

--- sanity.js
+++ sanity.js
@@ -1,16 +1,17 @@
 import { createClient } from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 
-createClient({
+export const client = createClient({
   projectId: "g7394wxi",
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-05-03",
 });
 
-export const client = createClient({
-  projectId: "your-project-id",
-  dataset: "your-dataset-name",
-  useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
-  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
-});
+// helper function to get image url
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+export default client;
+cors;

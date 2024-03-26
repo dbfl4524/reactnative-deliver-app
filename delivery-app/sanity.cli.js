bc71f3db61319a764514556a99286ec8d80f30be
dbfl4524 <sinyuri0525@gmail.com>
2024-03-26 09:56:43 +0900

    update

--- delivery-app/sanity.cli.js
+++ delivery-app/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'g7394wxi',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
---
+++ sanity.js
@@ -0,0 +1,16 @@
+import { createClient } from "@sanity/client";
+
+createClient({
+  projectId: "g7394wxi",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2023-05-03",
+});
+
+export const client = createClient({
+  projectId: "your-project-id",
+  dataset: "your-dataset-name",
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+});
